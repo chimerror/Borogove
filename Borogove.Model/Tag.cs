8a6a0d125acb53a85e67fbfcfa0780b4a4cfe159
foobardog <foobardog@hotmail.com>
2015-10-06 06:30:42 -0700

    Partial tag implementation, probably doesn't build.

--- Borogove.Model/Tag.cs
+++ Borogove.Model/Tag.cs
@@ -7,18 +7,24 @@ using System.Threading.Tasks;
 
 namespace Borogove.Model
 {
-    public class Tag
+    public sealed class Tag : IEquatable<Tag>
     {
-        public const string AliasSeparator = "=";
-        public const string ImplicationSeparator = "->";
         public const char WhitespaceReplacement = '_';
-        public const char ListSeparator = ',';
-
         public static readonly Regex WhitespaceRegex = new Regex(@"[\s_]+");
 
-        public string Name { get; set; }
-        public HashSet<string> Aliases { get; set; }
-        public List<Tag> Implications { get; set; }
+        internal Tag(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            Name = CanonicalizeTagName(name);
+        }
+
+        public string Name { get; }
+        public IEnumerable<string> Aliases { get; internal set; }
+        public IEnumerable<Tag> Implications { get; internal set; }
 
         public static string CanonicalizeTagName(string tagName)
         {
@@ -27,31 +33,92 @@ namespace Borogove.Model
                 throw new ArgumentNullException(nameof(tagName));
             }
 
-            string canonicalizedTagName =
-                WhitespaceRegex.Replace(tagName.ToLowerInvariant().Trim(), WhitespaceReplacement.ToString())
-                .Trim(WhitespaceReplacement);
-            if (string.IsNullOrWhiteSpace(canonicalizedTagName))
+            string canonicalizedTagName = CanonicalizeTagSpecification(tagName);
+
+            if (canonicalizedTagName.Contains(TagSet.AliasSeparator))
             {
-                throw new ArgumentException($"Canonicalized tag name cannot be null or whitespace. Supplied string was: ${tagName}");
+                throw new ArgumentException($"Canonicalized tag name cannot contain Alias Separator '${TagSet.AliasSeparator}'. Supplied string was: ${tagName}");
             }
 
-            if (canonicalizedTagName.Contains(AliasSeparator))
+            if (canonicalizedTagName.Contains(TagSet.ImplicationSeparator))
             {
-                throw new ArgumentException($"Canonicalized tag name cannot contain Alias Separator '${AliasSeparator}'. Supplied string was: ${tagName}");
-            }
-
-            if (canonicalizedTagName.Contains(ImplicationSeparator))
-            {
-                throw new ArgumentException($"Canonicalized tag name cannot contain Implication Separator '${ImplicationSeparator}'. Supplied string was: ${tagName}");
-            }
-
-            if (canonicalizedTagName.Contains(ListSeparator))
-            {
-                throw new ArgumentException($"Canonicalized tag name cannot contain List Separator '${ListSeparator}'. Supplied string was: ${tagName}");
+                throw new ArgumentException($"Canonicalized tag name cannot contain Implication Separator '${TagSet.ImplicationSeparator}'. Supplied string was: ${tagName}");
             }
 
             return canonicalizedTagName;
+        }
 
+        public static string CanonicalizeTagSpecification(string tagSpecification)
+        {
+            if (string.IsNullOrWhiteSpace(tagSpecification))
+            {
+                throw new ArgumentNullException(nameof(tagSpecification));
+            }
+
+            string canonicalizedTagSpecification =
+                WhitespaceRegex.Replace(tagSpecification.ToLowerInvariant().Trim(), WhitespaceReplacement.ToString())
+                .Trim(WhitespaceReplacement);
+
+            if (string.IsNullOrWhiteSpace(canonicalizedTagSpecification))
+            {
+                throw new ArgumentException($"Canonicalized tag specification cannot be null or whitespace. Supplied string was: ${tagSpecification}");
+            }
+
+            if (canonicalizedTagSpecification.Contains(TagSet.ListSeparator))
+            {
+                throw new ArgumentException($"Canonicalized tag specification cannot contain List Separator '${TagSet.ListSeparator}'. Supplied string was: ${tagSpecification}");
+            }
+
+            int aliasCount = canonicalizedTagSpecification.Count(c => c.Equals(TagSet.AliasSeparator));
+            int implicationCount = canonicalizedTagSpecification.Count(c => c.Equals(TagSet.ImplicationSeparator));
+            if (aliasCount > 0 && implicationCount > 0)
+            {
+                throw new ArgumentException($"Canonicalized tag specification cannot contain both Alias ('${TagSet.AliasSeparator}') and Implication ('${TagSet.ImplicationSeparator}') specifications. Supplied string was: ${tagSpecification}");
+            }
+
+            if (aliasCount != 0 && aliasCount != 1)
+            {
+                throw new ArgumentException($"Canonicalized tag specification cannot contain multiple Alias ('${TagSet.AliasSeparator}') specifications. Supplied string was: ${tagSpecification}");
+            }
+
+            if (implicationCount != 0 && implicationCount != 1)
+            {
+                throw new ArgumentException($"Canonicalized tag specification cannot contain multiple Implication ('${TagSet.ImplicationSeparator}') specifications. Supplied string was: ${tagSpecification}");
+            }
+
+            return canonicalizedTagSpecification;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Tag);
+        }
+
+        public bool Equals(Tag other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            // As this class is sealed, we can already assume that the names have been canonicalized, so we can just
+            // check that they are equal.
+            return other.Name.Equals(Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Borogove Tag '{Name}'";
         }
     }
 }
--- Borogove.Model/TagSet.cs
+++ Borogove.Model/TagSet.cs
@@ -4,11 +4,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Borogove.Model
 {
     public class TagSet : IEnumerable<Tag>
     {
+        public const char AliasSeparator = '=';
+        public const char ImplicationSeparator = '>';
+        public const char ListSeparator = ',';
+
+        private static readonly char[] AliasSeparatorArray = new char[] { AliasSeparator };
+        private static readonly char[] ImplicationSeparatorArray = new char[] { ImplicationSeparator };
+        private static readonly char[] ListSeparatorArray = new char[] { ListSeparator };
+
+        public TagSet(string initialTagList) : this(SplitTagList(initialTagList))
+        {
+        }
+
+        public TagSet(IEnumerable<string> initialTagList = null)
+        {
+            if (initialTagList != null)
+            {
+                foreach(string tag in initialTagList)
+                {
+                    ResolveTag(tag, true);
+                }
+            }
+        }
+
+        public static IEnumerable<string> SplitTagList(string tagList)
+        {
+            return tagList?.Split(ListSeparatorArray, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+        public IEnumerable<Tag> ResolveTagList(string tagList, bool updateTagSet = false)
+        {
+            return SplitTagList(tagList)?.SelectMany(ts => ResolveTag(ts, updateTagSet));
+        }
+
+        public IEnumerable<Tag> ResolveTag(string tagString, bool updateTagSet = false, bool resolveImplications = true)
+        {
+            if (string.IsNullOrWhiteSpace(tagString))
+            {
+                throw new ArgumentNullException(nameof(tagString));
+            }
+
+            string canonicalizedTagSpecification = Tag.CanonicalizeTagSpecification(tagString);
+            bool hasAlias = canonicalizedTagSpecification.Contains(AliasSeparator);
+            bool hasImplication = canonicalizedTagSpecification.Contains(ImplicationSeparator);
+
+            if (hasAlias)
+            {
+                List<string> splitAlias = canonicalizedTagSpecification
+                    .Split(AliasSeparatorArray, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => Tag.CanonicalizeTagName(s))
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .ToList();
+
+                if (splitAlias.Count != 2)
+                {
+                    throw new ArgumentException($"Invalid alias specification supplied: Supplied string was: ${tagString}");
+                }
+
+                // TODO: Implement
+            }
+            else if (hasImplication)
+            {
+                List<string> splitImplication = canonicalizedTagSpecification
+                    .Split(ImplicationSeparatorArray, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => Tag.CanonicalizeTagName(s))
+                    .Where(s => !string.IsNullOrWhiteSpace(s))
+                    .ToList();
+
+                if (splitImplication.Count != 2)
+                {
+                    throw new ArgumentException($"Invalid Implication specification supplied: Supplied string was: ${tagString}");
+                }
+
+                // TODO: Implement
+            }
+            else
+            {
+                string canonicalizedTagName = Tag.CanonicalizeTagName(tagString);
+            }
+        }
+
         public IEnumerator<Tag> GetEnumerator()
         {
             throw new NotImplementedException();
