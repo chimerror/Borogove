6e9082f81257874df9f32b771577e157f55cd713
foobardog <foobardog@hotmail.com>
2015-11-21 12:04:55 -0800

    First Implementation of TagSet

--- Borogove.Model/TagSet.cs
+++ Borogove.Model/TagSet.cs
@@ -2,9 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 
 namespace Borogove.Model
 {
@@ -19,7 +16,7 @@ namespace Borogove.Model
         private static readonly char[] ListSeparatorArray = new char[] { ListSeparator };
 
         private readonly Dictionary<string, Tag> _tagDictionary = new Dictionary<string, Tag>();
-        private readonly Dictionary<string, Tag> _implicationDictionary = new Dictionary<string, Tag>();
+        private readonly Dictionary<Tag, HashSet<Tag>> _implicationDictionary = new Dictionary<Tag, HashSet<Tag>>();
 
         public TagSet(string initialTagList) : this(SplitTagList(initialTagList))
         {
@@ -41,13 +38,17 @@ namespace Borogove.Model
             return tagList?.Split(ListSeparatorArray, StringSplitOptions.RemoveEmptyEntries);
         }
 
-
-        public IEnumerable<Tag> ResolveTagList(string tagList, bool updateTagSet = false)
+        public IEnumerable<Tag> ResolveTagList(string tagList, bool updateTagSet = false, bool resolveImplications = true)
         {
-            return SplitTagList(tagList)?.SelectMany(ts => ResolveTag(ts, updateTagSet));
+            var result = SplitTagList(tagList)?.Select(ts => ResolveTag(ts, updateTagSet)).Distinct();
+            if (resolveImplications)
+            {
+                result = result.Concat(result.SelectMany(t => t.Implications)).Distinct();
+            }
+            return result;
         }
 
-        public IEnumerable<Tag> ResolveTag(string tagString, bool updateTagSet = false, bool resolveImplications = true)
+        public Tag ResolveTag(string tagString, bool updateTagSet = false)
         {
             if (string.IsNullOrWhiteSpace(tagString))
             {
@@ -57,6 +58,7 @@ namespace Borogove.Model
             string canonicalizedTagSpecification = Tag.CanonicalizeTagSpecification(tagString);
             bool hasAlias = canonicalizedTagSpecification.Contains(AliasSeparator);
             bool hasImplication = canonicalizedTagSpecification.Contains(ImplicationSeparator);
+            Tag resultTag = null;
 
             if (hasAlias)
             {
@@ -68,25 +70,51 @@ namespace Borogove.Model
 
                 if (splitAlias.Count != 2)
                 {
-                    throw new ArgumentException($"Invalid alias specification supplied: Supplied string was: ${tagString}");
+                    throw new ArgumentException($"Invalid alias specification supplied: Supplied string was: {tagString}");
                 }
 
                 string canonicalizedAliasName = splitAlias[0];
                 Tag aliasTag;
                 bool aliasExists = _tagDictionary.TryGetValue(canonicalizedAliasName, out aliasTag);
                 string canonicalizedResultTagName = splitAlias[1];
-                Tag resultTag;
                 bool resultTagExists = _tagDictionary.TryGetValue(canonicalizedResultTagName, out resultTag);
 
                 if (updateTagSet)
                 {
                     if (aliasExists && resultTagExists)
                     {
-                        if (aliasTag.Equals(resultTag))
+                        if (!aliasTag.Equals(resultTag))
                         {
+                            _tagDictionary[canonicalizedAliasName] = resultTag;
                         }
                     }
+                    else if (resultTagExists)
+                    {
+                        _tagDictionary[canonicalizedAliasName] = resultTag;
+                    }
+                    else if (aliasExists)
+                    {
+                        // Favor the existing tag
+                        _tagDictionary[canonicalizedResultTagName] = aliasTag;
+                        resultTag = aliasTag;
+                    }
+                    else
+                    {
+                        resultTag = new Tag(canonicalizedResultTagName);
+                        _tagDictionary[canonicalizedResultTagName] = resultTag;
+                        _tagDictionary[canonicalizedAliasName] = resultTag;
+                        resultTag.Aliases = GetAliases(resultTag);
+                        resultTag.Implications = GetImplications(resultTag);
+                    }
                 }
+                else
+                {
+                    if (!aliasExists || !resultTagExists || !aliasTag.Equals(resultTag))
+                    {
+                        throw new InvalidOperationException($"Could not resolve alias specification without updating: ${tagString}");
+                    }
+                }
+
             }
             else if (hasImplication)
             {
@@ -98,25 +126,143 @@ namespace Borogove.Model
 
                 if (splitImplication.Count != 2)
                 {
-                    throw new ArgumentException($"Invalid Implication specification supplied: Supplied string was: ${tagString}");
+                    throw new ArgumentException($"Invalid Implication specification supplied: Supplied string was: {tagString}");
                 }
 
-                // TODO: Implement
+                string canonicalizedResultTagName = splitImplication[0];
+                bool resultTagExists = _tagDictionary.TryGetValue(canonicalizedResultTagName, out resultTag);
+                bool implicationsExist = resultTagExists ? _implicationDictionary.ContainsKey(resultTag) : false;
+                string canonicalizedImpliedTagName = splitImplication[1];
+                Tag impliedTag;
+                bool impliedTagExists = _tagDictionary.TryGetValue(canonicalizedImpliedTagName, out impliedTag);
+                bool impliedTagConnected = (impliedTagExists && implicationsExist)
+                    ? _implicationDictionary[resultTag].Contains(impliedTag)
+                    : false;
+
+                if (!impliedTagConnected)
+                {
+                    if (!updateTagSet)
+                    {
+                        throw new InvalidOperationException($"Could not resolve implication specification without updating: {tagString}");
+                    }
+
+                    if (!resultTagExists)
+                    {
+                        resultTag = new Tag(canonicalizedResultTagName);
+                        _tagDictionary[canonicalizedResultTagName] = resultTag;
+                        resultTag.Aliases = GetAliases(resultTag);
+                        resultTag.Implications = GetImplications(resultTag);
+                    }
+
+                    if (!impliedTagExists)
+                    {
+                        impliedTag = new Tag(canonicalizedImpliedTagName);
+                        _tagDictionary[canonicalizedImpliedTagName] = impliedTag;
+                        impliedTag.Aliases = GetAliases(impliedTag);
+                        impliedTag.Implications = GetImplications(impliedTag);
+                    }
+
+                    if (!implicationsExist)
+                    {
+                        _implicationDictionary.Add(resultTag, new HashSet<Tag>());
+                    }
+
+                    // Since it's a hash set, no error if we add it twice.
+                    _implicationDictionary[resultTag].Add(impliedTag);
+                }
             }
             else
             {
                 string canonicalizedTagName = Tag.CanonicalizeTagName(tagString);
+                bool tagExists = _tagDictionary.TryGetValue(canonicalizedTagName, out resultTag);
+                if (!tagExists && updateTagSet)
+                {
+                    resultTag = new Tag(canonicalizedTagName);
+                    _tagDictionary[canonicalizedTagName] = resultTag;
+                    resultTag.Aliases = GetAliases(resultTag);
+                    resultTag.Implications = GetImplications(resultTag);
+                }
+            }
+
+            if (resultTag == null)
+            {
+                throw new InvalidOperationException($"Unable to resolve tag specification: {tagString}");
+            }
+
+            return resultTag;
+        }
+
+        private IEnumerable<string> GetAliases(Tag tag)
+        {
+            if (tag == null)
+            {
+                throw new ArgumentNullException(nameof(tag));
+            }
+
+            if (!_tagDictionary.ContainsKey(tag.Name))
+            {
+                throw new ArgumentException($"Unrecognized tag provided: {tag.Name}");
+            }
+
+            var tagName = tag.Name;
+            var rootTag = _tagDictionary[tagName];
+            return _tagDictionary.Where(kvp => kvp.Value == rootTag && kvp.Key != tagName).Select(kvp => kvp.Key).Distinct();
+
+        }
+
+        private IEnumerable<Tag> GetImplications(Tag tag)
+        {
+            if (tag == null)
+            {
+                throw new ArgumentNullException(nameof(tag));
+            }
+
+            if (!_implicationDictionary.ContainsKey(tag))
+            {
+                yield break;
+            }
+
+            var stack = new Stack<Tag>();
+            foreach (var implication in _implicationDictionary[tag])
+            {
+                stack.Push(implication);
+            }
+
+            var seenTags = new HashSet<Tag>();
+            while (stack.Count != 0)
+            {
+                var current = stack.Pop();
+                if (seenTags.Contains(current))
+                {
+                    continue;
+                }
+
+                yield return current;
+                seenTags.Add(current);
+
+                if (!_implicationDictionary.ContainsKey(current))
+                {
+                    continue;
+                }
+
+                foreach (var implication in _implicationDictionary[current])
+                {
+                    if (!implication.Equals(tag) && !seenTags.Contains(implication))
+                    {
+                        stack.Push(implication);
+                    }
+                }
             }
         }
 
         public IEnumerator<Tag> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return _tagDictionary.Values.Distinct().GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
     }
 }
---
+++ Borogove.Tests/TagSetTests.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Borogove.Model;
+
+namespace Borogove.Tests
+{
+    [TestFixture]
+    public class TagSetTests
+    {
+        [Test]
+        public void CanAddAndResolveSingleBareTag()
+        {
+            var tagString = "Music";
+            var canonicalizedTagName = Tag.CanonicalizeTagName(tagString);
+            var target = new TagSet(tagString);
+
+            var result = target.ResolveTag(tagString);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Name, Is.EqualTo(canonicalizedTagName));
+
+            var resultAliases = result.Aliases.ToList();
+            Assert.That(resultAliases, Is.Empty);
+
+            var resultImplications = result.Implications.ToList();
+            Assert.That(resultImplications, Is.Empty);
+        }
+
+        [Test]
+        public void CanAddAndResolveMultipleBareTags()
+        {
+            var tagString = "Wine, Women,song";
+            var target = new TagSet(tagString);
+
+            var inputStrings = new string[] { "Wine", "Women", "Song" };
+
+            foreach (var inputString in inputStrings)
+            {
+                var canonicalizedTagName = Tag.CanonicalizeTagName(inputString);
+                var result = target.ResolveTag(inputString);
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Name, Is.EqualTo(canonicalizedTagName));
+
+                var resultAliases = result.Aliases.ToList();
+                Assert.That(resultAliases, Is.Empty);
+
+                var resultImplications = result.Implications.ToList();
+                Assert.That(resultImplications, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void CanAddAndResolveSingleAliasSpecification()
+        {
+            var tagString = "Puma = Cougar";
+            var expectedAliasName = Tag.CanonicalizeTagName("Puma");
+            var expectedTagName = Tag.CanonicalizeTagName("Cougar");
+            var target = new TagSet(tagString);
+
+            var result1 = target.ResolveTag("Puma");
+            Assert.That(result1, Is.Not.Null);
+            Assert.That(result1.Name, Is.EqualTo(expectedTagName));
+
+            var resultAliases1 = result1.Aliases.ToList();
+            Assert.That(resultAliases1, Has.Count.EqualTo(1));
+            Assert.That(resultAliases1.First(), Is.EqualTo(expectedAliasName));
+
+            var resultImplications1 = result1.Implications.ToList();
+            Assert.That(resultImplications1, Is.Empty);
+
+            var result2 = target.ResolveTag("Cougar");
+            Assert.That(result2, Is.Not.Null);
+            Assert.That(result2.Name, Is.EqualTo(expectedTagName));
+
+            var resultAliases2 = result2.Aliases.ToList();
+            Assert.That(resultAliases2, Has.Count.EqualTo(1));
+            Assert.That(resultAliases2.First(), Is.EqualTo(expectedAliasName));
+
+            var resultImplications2 = result2.Implications.ToList();
+            Assert.That(resultImplications2, Is.Empty);
+        }
+
+        [Test]
+        public void CanAddAndResolveMultipleAliasSpecifications()
+        {
+            var tagString = "Puma=Cougar, Panther = Cougar, Cougar = Catamount";
+            var target = new TagSet(tagString);
+            var expectedTagName = Tag.CanonicalizeTagName("Cougar");
+            var expectedAliasNames = new string[] { "Puma", "panther", "Catamount" }.Select(a => Tag.CanonicalizeTagName(a)).ToList();
+
+            var resultTag = target.ResolveTag("cougar");
+            Assert.That(resultTag, Is.Not.Null);
+            Assert.That(resultTag.Name, Is.EqualTo(expectedTagName));
+
+            var resultAliases = resultTag.Aliases.ToList();
+            Assert.That(resultAliases, Is.EquivalentTo(expectedAliasNames));
+
+            var resultImplications = resultTag.Implications.ToList();
+            Assert.That(resultImplications, Is.Empty);
+
+            foreach (var aliasString in expectedAliasNames)
+            {
+                var aliasResult = target.ResolveTag(aliasString);
+                Assert.That(aliasResult, Is.Not.Null);
+                Assert.That(aliasResult.Name, Is.EqualTo(expectedTagName));
+
+                var aliasAliases = aliasResult.Aliases.ToList();
+                Assert.That(aliasAliases, Is.EquivalentTo(expectedAliasNames));
+
+                var aliasImplications = aliasResult.Implications.ToList();
+                Assert.That(aliasImplications, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void CanAddAndResolveSingleImplicationSpecification()
+        {
+            var tagString = "Mammal > Animal";
+            var expectedTagName = Tag.CanonicalizeTagName("Mammal");
+            var expectedImplicationName = Tag.CanonicalizeTagName("Animal");
+            var target = new TagSet(tagString);
+
+            var result = target.ResolveTag("Mammal");
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Name, Is.EqualTo(expectedTagName));
+
+            var resultAliases = result.Aliases.ToList();
+            Assert.That(resultAliases, Is.Empty);
+
+            var resultImplications = result.Implications.ToList();
+            Assert.That(resultImplications, Has.Count.EqualTo(1));
+
+            var impliedTag = resultImplications.First();
+            Assert.That(impliedTag, Is.Not.Null);
+            Assert.That(impliedTag.Name, Is.EqualTo(expectedImplicationName));
+
+            var impliedAliases = impliedTag.Aliases.ToList();
+            Assert.That(impliedAliases, Is.Empty);
+
+            var impliedImplications = impliedTag.Implications.ToList();
+            Assert.That(impliedImplications, Is.Empty);
+
+            var directImpliedTag = target.ResolveTag("Animal");
+            Assert.That(directImpliedTag, Is.Not.Null);
+            Assert.That(directImpliedTag.Name, Is.EqualTo(expectedImplicationName));
+
+            var directImpliedAliases = directImpliedTag.Aliases.ToList();
+            Assert.That(directImpliedAliases, Is.Empty);
+
+            var directImpliedImplications = directImpliedTag.Implications.ToList();
+            Assert.That(directImpliedImplications, Is.Empty);
+        }
+
+        [Test]
+        public void CanAddAndResolveMultipleImplicationSpecifications()
+        {
+            var tagString = "Mammal > Animal,Canine>Mammal, Feline > mammal";
+            var expectedFirstTagName = Tag.CanonicalizeTagName("Canine");
+            var expectedSecondTagName = Tag.CanonicalizeTagName("Feline");
+            var expectedFirstImplicationName = Tag.CanonicalizeTagName("Mammal");
+            var expectedSecondImplicationName = Tag.CanonicalizeTagName("Animal");
+            var target = new TagSet(tagString);
+
+            var firstResult = target.ResolveTag("canine");
+            Assert.That(firstResult, Is.Not.Null);
+            Assert.That(firstResult.Name, Is.EqualTo(expectedFirstTagName));
+
+            var firstAliases = firstResult.Aliases.ToList();
+            Assert.That(firstAliases, Is.Empty);
+
+            var firstImplications = firstResult.Implications.Select(i => i.Name).ToList();
+            Assert.That(firstImplications, Has.Count.EqualTo(2));
+            var expectedFirstImplications = new List<string>() { expectedFirstImplicationName, expectedSecondImplicationName };
+            Assert.That(firstImplications, Is.EquivalentTo(expectedFirstImplications));
+
+            var secondResult = target.ResolveTag("Feline");
+            Assert.That(secondResult, Is.Not.Null);
+            Assert.That(secondResult.Name, Is.EqualTo(expectedSecondTagName));
+
+            var secondAliases = secondResult.Aliases.ToList();
+            Assert.That(secondAliases, Is.Empty);
+
+            var secondImplications = secondResult.Implications.Select(i => i.Name).ToList();
+            Assert.That(secondImplications, Has.Count.EqualTo(2));
+            var expectedSecondImplications = new List<string>() { expectedFirstImplicationName, expectedSecondImplicationName };
+            Assert.That(secondImplications, Is.EquivalentTo(expectedSecondImplications));
+        }
+
+        [Test]
+        public void CanAddAndResolveCircularImplicationSpecifications()
+        {
+            var tagString = "foo > bar, bar > baz, baz > foo";
+            var expectedFirstTagName = Tag.CanonicalizeTagName("foo");
+            var expectedSecondTagName = Tag.CanonicalizeTagName("bar");
+            var expectedThirdTagName = Tag.CanonicalizeTagName("baz");
+            var expectedTagNames = new List<string>() { expectedFirstTagName, expectedSecondTagName, expectedThirdTagName };
+            var target = new TagSet(tagString);
+
+            foreach (string expectedTagName in expectedTagNames)
+            {
+                var actualTag = target.ResolveTag(expectedTagName);
+                Assert.That(actualTag, Is.Not.Null);
+                Assert.That(actualTag.Name, Is.EqualTo(expectedTagName));
+
+                var actualTagAliases = actualTag.Aliases.ToList();
+                Assert.That(actualTagAliases, Is.Empty);
+
+                var actualTagImplications = actualTag.Implications.ToList();
+                Assert.That(actualTagImplications.Select(t => t.Name), Is.EquivalentTo(expectedTagNames.Where(tn => !tn.Equals(expectedTagName))));
+            }
+        }
+
+        [Test]
+        public void CanAddAndResolveMixedSpecifications()
+        {
+            var tagString = "Puma, Mountain Lion = Puma, Puma > Feline, Canine";
+            var expectedRootTagName = Tag.CanonicalizeTagName("puma");
+            var expectedAlias = Tag.CanonicalizeTagName("__mountain__Lion__");
+            var expectedImplicationTagName = Tag.CanonicalizeTagName("feline");
+            var expectedOtherTagName = Tag.CanonicalizeTagName("canine");
+            var target = new TagSet(tagString);
+
+            var rootResult = target.ResolveTag("mountain lion");
+            Assert.That(rootResult, Is.Not.Null);
+            Assert.That(rootResult.Name, Is.EqualTo(expectedRootTagName));
+
+            var rootAliases = rootResult.Aliases.ToList();
+            Assert.That(rootAliases, Has.Count.EqualTo(1));
+            Assert.That(rootAliases.First(), Is.EqualTo(expectedAlias));
+
+            var rootImplications = rootResult.Implications.ToList();
+            Assert.That(rootImplications, Has.Count.EqualTo(1));
+
+            var implicationTag = rootImplications.First();
+            Assert.That(implicationTag, Is.Not.Null);
+            Assert.That(implicationTag.Name, Is.EqualTo(expectedImplicationTagName));
+
+            var implicationAliases = implicationTag.Aliases.ToList();
+            Assert.That(implicationAliases, Is.Empty);
+
+            var implicationImplications = implicationTag.Implications.ToList();
+            Assert.That(implicationImplications, Is.Empty);
+
+            var otherTag = target.ResolveTag("Canine");
+            Assert.That(otherTag, Is.Not.Null);
+            Assert.That(otherTag.Name, Is.EqualTo(expectedOtherTagName));
+
+            var otherAliases = otherTag.Aliases.ToList();
+            Assert.That(otherAliases, Is.Empty);
+
+            var otherImplications = otherTag.Implications.ToList();
+            Assert.That(otherImplications, Is.Empty);
+        }
+
+        [Test]
+        public void CanResolveTagStringToRootsWithUpdates()
+        {
+            var tagString = "Puma, Mountain Lion = Puma, Puma > Feline, Canine";
+            var expectedRootTagName = Tag.CanonicalizeTagName("puma");
+            var expectedAlias = Tag.CanonicalizeTagName("mountain lion");
+            var expectedImplicationTagName = Tag.CanonicalizeTagName("feline");
+            var expectedOtherTagName = Tag.CanonicalizeTagName("canine");
+            var expectedResultTagNames = new List<string>() { expectedRootTagName, expectedOtherTagName };
+            var target = new TagSet();
+
+            var result = target.ResolveTagList(tagString, true, false).ToList();
+            Assert.That(result, Has.Count.EqualTo(2));
+            Assert.That(result.Select(t => t.Name), Is.EquivalentTo(expectedResultTagNames));
+
+            var rootTag = result.First(t => t.Name.Equals(expectedRootTagName));
+            var rootTagAliases = rootTag.Aliases.ToList();
+            Assert.That(rootTagAliases, Has.Count.EqualTo(1));
+            Assert.That(rootTagAliases.First(), Is.EqualTo(expectedAlias));
+
+            var rootTagImplications = rootTag.Implications.ToList();
+            Assert.That(rootTagImplications, Has.Count.EqualTo(1));
+
+            var implicationTag = rootTagImplications.First();
+            Assert.That(implicationTag, Is.Not.Null);
+            Assert.That(implicationTag.Name, Is.EqualTo(expectedImplicationTagName));
+
+            var implicationAliases = implicationTag.Aliases.ToList();
+            Assert.That(implicationAliases, Is.Empty);
+
+            var implicationImplications = implicationTag.Implications.ToList();
+            Assert.That(implicationImplications, Is.Empty);
+
+            var otherTag = result.First(t => t.Name.Equals(expectedOtherTagName));
+            var otherAliases = otherTag.Aliases.ToList();
+            Assert.That(otherAliases, Is.Empty);
+
+            var otherImplications = otherTag.Implications.ToList();
+            Assert.That(otherImplications, Is.Empty);
+        }
+
+        [Test]
+        public void CanResolveTagStringToImplicationsWithUpdates()
+        {
+            var tagString = "Puma, Mountain Lion = Puma, Puma > Feline, Canine";
+            var expectedRootTagName = Tag.CanonicalizeTagName("puma");
+            var expectedAlias = Tag.CanonicalizeTagName("mountain lion");
+            var expectedImplicationTagName = Tag.CanonicalizeTagName("feline");
+            var expectedOtherTagName = Tag.CanonicalizeTagName("canine");
+            var expectedResultTagNames = new List<string>() { expectedRootTagName, expectedOtherTagName, expectedImplicationTagName };
+            var target = new TagSet();
+
+            var result = target.ResolveTagList(tagString, true, true).ToList();
+            Assert.That(result, Has.Count.EqualTo(3));
+            Assert.That(result.Select(t => t.Name), Is.EquivalentTo(expectedResultTagNames));
+
+            var rootTag = result.First(t => t.Name.Equals(expectedRootTagName));
+            var rootTagAliases = rootTag.Aliases.ToList();
+            Assert.That(rootTagAliases, Has.Count.EqualTo(1));
+            Assert.That(rootTagAliases.First(), Is.EqualTo(expectedAlias));
+
+            var rootTagImplications = rootTag.Implications.ToList();
+            Assert.That(rootTagImplications, Has.Count.EqualTo(1));
+
+            var implicationTag = rootTagImplications.First();
+            Assert.That(implicationTag, Is.Not.Null);
+            Assert.That(implicationTag.Name, Is.EqualTo(expectedImplicationTagName));
+            Assert.That(implicationTag, Is.EqualTo(result.First(t => t.Name.Equals(expectedImplicationTagName))));
+
+            var implicationAliases = implicationTag.Aliases.ToList();
+            Assert.That(implicationAliases, Is.Empty);
+
+            var implicationImplications = implicationTag.Implications.ToList();
+            Assert.That(implicationImplications, Is.Empty);
+
+            var otherTag = result.First(t => t.Name.Equals(expectedOtherTagName));
+            var otherAliases = otherTag.Aliases.ToList();
+            Assert.That(otherAliases, Is.Empty);
+
+            var otherImplications = otherTag.Implications.ToList();
+            Assert.That(otherImplications, Is.Empty);
+        }
+    }
+}
