5d9dc9110dc5841a702dfa986f0a2447f57cfd57
foobardog <foobardog@hotmail.com>
2016-02-08 07:52:33 -0800

    Adding TagAliasesController.

---
+++ Borogove.API/Controllers/TagAliasesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.OData;
+using System.Web.OData.Routing;
+using Borogove.DataAccess;
+
+namespace Borogove.API.Controllers
+{
+    [ODataRoutePrefix("TagAliases")]
+    public class TagAliasesController : ODataController
+    {
+        WorkContext db = new WorkContext();
+
+        [EnableQuery]
+        public IQueryable<TagAliasEntity> Get()
+        {
+            return db.TagAliases;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
